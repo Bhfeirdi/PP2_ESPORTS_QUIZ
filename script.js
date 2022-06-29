c44ce3fa4096ee8f27736176ba27d577dea4b1bd
Bhfeirdi <ferdia.link@gmail.com>
2022-06-29 03:11:23 +0100

    commenting out sections for debugging

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
+/*
 const playButton = document.getElementById("play_button")
 const nextButton = document.getElementById("next_button")
 const quizContainer = document.getElementById("quiz_container")
 const questionElement = document.getElementById("random_question")
 const answerButtonElement = document.getElementById("answers")
+
 let questionRandomiser, currentQuestions
 
 
 playButton.addEventListener("click", playGame)
 nextButton.addEventListener("click", () => {
-    currentQuestions++
+    currentQuestions ++
     selectNextQuestion()
 })
 
 function playGame() {
     playButton.classList.add("hide")
-    questionRandomiser = myQuestions.sort(() => Math.random() -.5)
+    questionRandomiser = random_question.sort(() => Math.random() -.5)
     currentQuestions = 0
     quizContainer.classList.remove("hide")
     selectNextQuestion()
@@ -76,12 +78,91 @@ function clearStatusClass(element) {
     element.classList.remove("correct")
     element.classList.remove("wrong")
 }
+*/
+
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
 
 
 
 //-------------------------------------- QUESTIONS ARRAY----------------------------------------------------//
 
-const myQuestions = [
+const questions = [
     {
         question: 'What is the name of the only team in DOTA 2 history to win "The Aegis of Champions", the highest honor in competitive Dota?',
         answers: [
